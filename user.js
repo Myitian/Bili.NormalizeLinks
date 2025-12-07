e11972d5b3e013507afd78e842876b3e1300ce84
Myitian <miaoyitian233@gmail.com>
2025-12-07 20:55:44 +0800

    v0.2

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         规范化链接 - 替换Bilibili部分链接为带href的常规a元素
 // @namespace    myitian.js.bili.NormalizeLinks
-// @version      0.1
+// @version      0.2
 // @description  替换Bilibili部分链接为带href的常规a元素，以提升可访问性，获得原生链接的体验（如右键菜单的“在新标签页打开链接”“在新窗口打开链接”等选项）。
 // @source       https://github.com/Myitian/Bili.NormalizeLinks
 // @author       Myitian
@@ -43,18 +43,21 @@ unsafeWindow.open = function (url, ...args) {
  */
 EventTarget.prototype.addEventListener = function (type, callback, ...args) {
   try {
+    let mayHaveData = false;
     if (type === 'click') {
       callState.special = predicate(callback);
       if (callState.special) {
         // 使用假执行来获取真实链接
         if (typeof callback === 'function') {
           callback.call(this ?? window, dummyEvent);
+          mayHaveData = true;
         } else if (typeof callback === 'object' && callback.handleEvent) {
           callback.handleEvent(dummyEvent);
+          mayHaveData = true;
         }
       }
     }
-    if (callState.url !== "" && this instanceof Element) {
+    if (mayHaveData && callState.url !== "" && this instanceof Element) {
       console.log(`[BiliNrmLnk] Found URL: ${callState.url}`);
       if (tryInstantReplace && this.isConnected) {
         replaceUrl(changeElementToAnchor(this), callState.url);
@@ -69,59 +72,49 @@ EventTarget.prototype.addEventListener = function (type, callback, ...args) {
     callState.special = false;
   }
 }
+
+replace();
+window.addEventListener('mousedown', replace);
+window.addEventListener('keydown', replace);
+document.addEventListener('DOMContentLoaded', replace);
+
 /**
- * 由于Bilibili采用了向非Anchor元素绑定闭包中的事件监听函数，这里尝试检查是否是可以处理的函数。这里可能随着Bilibili更新代码而不稳定，需要经常观察前端JS。
- * @param {*} func 
+ * 由于Bilibili采用了向非Anchor元素绑定闭包中的事件监听函数，这里尝试检查是否是可以处理的函数。
+ * 这里可能随着Bilibili更新代码而不稳定，需要经常观察前端JS。
+ * @param {*} func
  */
 function predicate(func) {
   return /&&\s*[^.]+.preventDefault\s*\(\s*\)\s*,\s*[^.]+.stopPropagation\s*\(\s*\)\s*,\s*window\.open\([^(]*\(\s*[^.]+\.click\.url\s*,\s*[^.]+\.click\.name\s*\)\s*\)\s*\)\s*/.test(`${func}`);
 }
 
-let running = false;
-let waitingCount = 0;
-const eventTarget = new EventTarget();
-const customEvent = new CustomEvent('proc_done');
-/** @type {Object<string,string>} */
-const defaultDisplayCache = {};
-/** @type {Set<string>} */
-const displayCache = new Set();
-
-replace();
-window.addEventListener('mousedown', replace);
-window.addEventListener('keydown', replace);
-
-/**
- * @param {EventTarget} target
- * @param {string} event
- */
-function getPromiseFromEvent(target, event) {
-  return new Promise((resolve) => {
-    const listener = () => {
-      target.removeEventListener(event, listener);
-      resolve();
-    };
-    target.addEventListener(event, listener);
-  });
-}
-
-async function replace() {
-  if (waitingCount > 2) {
-    console.log('[BiliNrmLnk] Too many waiting replacement. Canceled new replacement.');
-    return;
-  }
-  if (running) {
-    waitingCount++;
-    await getPromiseFromEvent(eventTarget, 'proc_done');
-    waitingCount--;
-  }
-  running = true;
+function replace() {
   if (pending.length > 0) {
     for (const it of pending) {
       replaceUrl(changeElementToAnchor(it[0]), it[1]);
     }
     pending.length = 0;
   }
-  // 旧版替换逻辑。其中部分内容可能已经无法用于现在的Bilibili，但这些代码应该不会造成负面问题。
+  let c = 0;
+  // 补充的特定目标修复——提供更准确的目标地址
+  /** @type {NodeListOf<HTMLAnchorElement>} */
+  const urlCollectionTitle = document.querySelectorAll('.video-pod a.title.jumpable:not([data-bilinrmlnk-replaced])');
+  for (const e of urlCollectionTitle) {
+    const m = /\/\/space\.bilibili\.com\/([^\/]+)\/channel\/collectiondetail\?(?:.+&)?sid=([^&]+)/.exec(e.href);
+    if (!m) continue;
+    replaceUrl(e, `https://space.bilibili.com/${m[1]}/lists/${m[2]}`, true);
+    c++;
+  }
+  // 旧版替换逻辑
+  c += legacyReplace();
+  if (c) {
+    console.log(`[BiliNrmLnk] Replacement completed. ${c} element(s) effected.`);
+  }
+}
+
+/**
+ * 旧版替换逻辑。其中部分内容可能已经无法用于现在的Bilibili，但这些代码应该不会造成负面问题。
+ */
+function legacyReplace() {
   let c = 0;
   /** @type {NodeListOf<HTMLAnchorElement>} */
   const urlA = document.querySelectorAll('a.jump-link[data-url]:not([data-bilinrmlnk-replaced])');
@@ -150,26 +143,32 @@ async function replace() {
     replaceUserId(changeElementToAnchor(e));
     c++;
   }
-  running = false;
-  console.log(`[BiliNrmLnk] Replacement completed. ${c} element(s) effected.`);
-  eventTarget.dispatchEvent(customEvent);
+  return c;
 }
 
 /**
- * @param {HTMLAnchorElement} e 
- * @param {string} newUrl 
+ * @param {HTMLAnchorElement} e
+ * @param {string} newUrl
+ * @param {boolean} lockSetAttributeHerf
  */
-function replaceUrl(e, newUrl = undefined) {
+function replaceUrl(e, newUrl = undefined, lockSetAttributeHerf = false) {
   e.href = newUrl ?? e.dataset.url;
   e.setAttribute("data-bilinrmlnk-replaced", "");
   e.target = '_blank';
   e.onclick = null;
   originalAddEventListener.call(e, 'click', stopImmediatePropagation, true);
+  if (lockSetAttributeHerf) {
+    const originalSetAttribute = e.setAttribute;
+    e.setAttribute = function (qualifiedName, value) {
+      if (qualifiedName === "href") return;
+      originalSetAttribute(qualifiedName, value);
+    }
+  }
 }
 
 /**
- * @param {HTMLAnchorElement} e 
- * @param {string} attr 
+ * @param {HTMLAnchorElement} e
+ * @param {string} attr
  */
 function replaceUserId(e, attr = 'data-user-id') {
   e.href = 'https://space.bilibili.com/' + e.getAttribute(attr);
@@ -197,6 +196,11 @@ function changeElementToAnchor(element) {
   p.insertBefore(ne, nxt);
   return ne;
 }
+
+/** @type {Object<string,string>} */
+const defaultDisplayCache = {};
+/** @type {Set<string>} */
+const displayCache = new Set();
 
 /**
  * @param {string} tagName
